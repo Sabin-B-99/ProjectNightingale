443376ed24724440971c2ea38010d9a8b4b10876
BadalSabin <55592534+BadalSabin@users.noreply.github.com>
2023-03-03 19:33:38 +0545

    Added Database Entity Title

---
+++ src/main/java/com/projectnight/entity/Topic.java
@@ -0,0 +1,34 @@
+package com.projectnight.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "topics")
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    public Topic() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
