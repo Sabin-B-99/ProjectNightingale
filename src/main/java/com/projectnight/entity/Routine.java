d26db64e446d80b08301681ade3d49d489d90450
BadalSabin <55592534+BadalSabin@users.noreply.github.com>
2023-03-10 14:52:39 +0545

    Implemented a routine entity to help user add a new practice routine

---
+++ src/main/java/com/projectnight/entity/Routine.java
@@ -0,0 +1,46 @@
+package com.projectnight.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "routine")
+public class Routine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @OneToMany(mappedBy = "routine")
+    private List<Topic> topics;
+
+    public Routine() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<Topic> topics) {
+        this.topics = topics;
+    }
+}
