839c5458ae359eb757cf2b25e833ac55d58337cf
BadalSabin <55592534+BadalSabin@users.noreply.github.com>
2023-03-11 14:43:58 +0545

    Added user entity for user authentication based on authority

---
+++ src/main/java/com/projectnight/entity/users/User.java
@@ -0,0 +1,57 @@
+package com.projectnight.entity.users;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<UserRole> userRole;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<UserRole> getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(List<UserRole> userRole) {
+        this.userRole = userRole;
+    }
+}
