97b4ab259e4967b60fb2d375ef03112b3214e87e
BadalSabin <55592534+BadalSabin@users.noreply.github.com>
2023-03-15 17:33:27 +0545

    Restored deleted file. No changes

---
+++ src/main/java/com/projectnight/configuration/ProjectNightingaleSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.projectnight.configuration;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class ProjectNightingaleSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
