400502d80cc6272d26f5657982886964ca4607de
BadalSabin <55592534+BadalSabin@users.noreply.github.com>
2023-03-15 17:32:48 +0545

    Removed security filter chains for now

--- src/main/java/com/projectnight/configuration/SecurityConfig.java
+++ src/main/java/com/projectnight/configuration/SecurityConfig.java
@@ -3,17 +3,18 @@ package com.projectnight.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity https) throws Exception{
-        https.cors().disable().csrf().disable().authorizeRequests().anyRequest();
-        return https.build();
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
