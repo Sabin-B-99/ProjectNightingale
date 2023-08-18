29572d6f4d287cf65a0cff4451113ef5d9deaf73
Sabin B <55592534+Sabin-B-99@users.noreply.github.com>
2023-08-18 19:56:09 +0545

    Chord name can be added to lyrics tab input area on chord button click

--- ProjectNightingaleFrontend/src/app/services/tab-creator.service.ts
+++ ProjectNightingaleFrontend/src/app/services/tab-creator.service.ts
@@ -1,4 +1,4 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import {ElementRef, EventEmitter, Injectable} from '@angular/core';
 import {Chord} from "../models/chord-model/chord";
 import {Subject} from "rxjs";
 
@@ -15,5 +15,68 @@ export class TabCreatorService {
     this.selectedChordsForTabs = selectedChords;
     this.selectedChordsChangedEvenEmitter.next(this.selectedChordsForTabs.slice());
     this.selectedChordsForTabs.splice(0, this.selectedChordsForTabs.length);
+  }
+  public setCursorAt(textArea: ElementRef<HTMLTextAreaElement>, lineNo:number, col: number){
+    let startPos: number = textArea.nativeElement.selectionStart;
+
+    let textAreaLinesTillCurrentCursorPos: string[] = textArea.nativeElement.value.substring(0, startPos)
+      .split('\n');
+
+    let cursorCurrentRow: number = textAreaLinesTillCurrentCursorPos.length;
+
+    if(cursorCurrentRow - lineNo > 1){
+      textArea.nativeElement.selectionStart =
+        this.findNumberOfCharacters(textAreaLinesTillCurrentCursorPos.slice(0, lineNo-1)) + 1;
+      textArea.nativeElement.selectionEnd = textArea.nativeElement.selectionStart + col;
+    }else if (cursorCurrentRow - lineNo < 1){
+      this.addLineBreaks(textArea, lineNo - cursorCurrentRow);
+      textArea.nativeElement.selectionEnd = textArea.nativeElement.selectionStart + col;
+    }else{
+      textArea.nativeElement.selectionStart = col;
+      textArea.nativeElement.selectionEnd = col;
+    }
+  }
+
+  public addLineBreaks(textAreaElem: ElementRef<HTMLTextAreaElement>, numOfBreaks: number){
+    for (let i = 0; i < numOfBreaks; i++) {
+      textAreaElem.nativeElement.value += '\n';
+    }
+  }
+
+  public findNumberOfCharacters(lines: string[]): number{
+    let totalNumberOfChars: number = 0;
+    for (const line of lines) {
+      totalNumberOfChars += line.length;
+    }
+    return totalNumberOfChars;
+  }
+
+  public calculateWordsInLyricsTab(tabTextAreaValue: string): number{
+    let texAfterWhiteSpaceRemoval: string = this.removeBlankLinesAfterRemovingTextsInParenthesis(tabTextAreaValue);
+    console.log(texAfterWhiteSpaceRemoval);
+    let totalWords: number = 0
+    let tabLines: string[] =  texAfterWhiteSpaceRemoval.split('\n');
+    for (const line of tabLines) {
+        totalWords += this.calculateTotalWordsInALine(line);
+    }
+    return totalWords;
+  }
+
+  private removeWordsInsideParenthesis(line: string): string{
+    return line.replace(/\s*\([^()]*\)$/, '');
+  }
+  private calculateTotalWordsInALine(line: string): number {
+    return line.split(new RegExp(/\b\s+/)).length;
+  }
+  private removeBlankLinesAfterRemovingTextsInParenthesis(textAreaValue: string): string{
+    let lines: string[] = textAreaValue.split('\n');
+    let remLinesAfterRemoval: string[] = [];
+    for (const line of lines) {
+      let lineAfterRemWordsInParenthesis = this.removeWordsInsideParenthesis(line);
+      if(lineAfterRemWordsInParenthesis.trim() !== ""){
+        remLinesAfterRemoval.push(lineAfterRemWordsInParenthesis);
+      }
+    }
+    return remLinesAfterRemoval.join('\n');
   }
 }
--- ProjectNightingaleFrontend/src/app/songs/song-tab-creator/song-tab-creator.component.ts
+++ ProjectNightingaleFrontend/src/app/songs/song-tab-creator/song-tab-creator.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {TabRequiredDetailsComponent} from "./tab-required-details/tab-required-details.component";
 import {TabCreatorService} from "../../services/tab-creator.service";
@@ -10,7 +10,14 @@ import {Chord} from "../../models/chord-model/chord";
   templateUrl: './song-tab-creator.component.html',
   styleUrls: ['./song-tab-creator.component.css']
 })
-export class SongTabCreatorComponent implements OnInit, OnDestroy{
+export class SongTabCreatorComponent implements OnInit, OnDestroy, AfterViewInit{
+
+  @ViewChild('lyricsTabArea') lyricsTabTextArea: ElementRef<HTMLTextAreaElement>;
+  readonly LYRICS_TEMPLATE_WORDS_SEP_LINES: number = 5;
+  readonly LYRICS_TEMPLATE_WORDS: string[] = ['(Intro)', '(Verse 1)', '(Verse 2)',
+    '(Chorus)', '(Bridge)', '(Chorus)'];
+  readonly LYRICS_AREA_NO_OF_ROWS: number = 20;
+  readonly LYRICS_AREA_NO_OF_COLS: number = 30;
 
   tabCreationForm: FormGroup;
 
@@ -34,6 +41,12 @@ export class SongTabCreatorComponent implements OnInit, OnDestroy{
           this.selectedChordsForTab = selectedChords
         }
       );
+
+  }
+
+  ngAfterViewInit() {
+    this.createTabTemplate();
+    this.tabCreatorService.setCursorAt(this.lyricsTabTextArea,2,0);
   }
 
   ngOnDestroy() {
@@ -43,7 +56,8 @@ export class SongTabCreatorComponent implements OnInit, OnDestroy{
   }
 
   onTabCreationFormSubmitted(){
-    console.log(this.tabCreationForm.value);
+    let totalWords: number = this.tabCreatorService.calculateWordsInLyricsTab(this.tabCreationForm.get('tabLyricsArea')?.value);
+    console.log(totalWords);
   }
 
   getTabRequiredDetailsForm(): FormGroup{
@@ -51,5 +65,24 @@ export class SongTabCreatorComponent implements OnInit, OnDestroy{
   }
 
   addSelectedChordInTab(selectedChord: Chord) {
+    let chordName: string = `${selectedChord.chordRoot.rootName}${selectedChord.chordKey.keyName}`;
+    if(this.lyricsTabTextArea.nativeElement.selectionStart || this.lyricsTabTextArea.nativeElement.selectionStart === 0){
+      let startPos: number =  this.lyricsTabTextArea.nativeElement.selectionStart;
+      let endPos: number = this.lyricsTabTextArea.nativeElement.selectionEnd;
+      this.lyricsTabTextArea.nativeElement.value = this.lyricsTabTextArea.nativeElement.value.substring(0, startPos) +
+        `[${chordName}]` + this.lyricsTabTextArea.nativeElement.value.substring(endPos,
+          this.lyricsTabTextArea.nativeElement.value.length);
+      this.lyricsTabTextArea.nativeElement.selectionStart = startPos + chordName.length;
+      this.lyricsTabTextArea.nativeElement.selectionEnd = endPos + chordName.length;
+    }else{
+      this.lyricsTabTextArea.nativeElement.value += this.lyricsTabTextArea.nativeElement.value;
+    }
+  }
+
+  private createTabTemplate(){
+    for (let word of this.LYRICS_TEMPLATE_WORDS) {
+      this.lyricsTabTextArea.nativeElement.value += `${word}`;
+      this.tabCreatorService.addLineBreaks(this.lyricsTabTextArea, this.LYRICS_TEMPLATE_WORDS_SEP_LINES);
+    }
   }
 }
