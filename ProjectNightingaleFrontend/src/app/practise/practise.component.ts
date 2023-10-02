94785ab4a2035ee6da2ff048902050080630384c
Sabin B <55592534+Sabin-B-99@users.noreply.github.com>
2023-10-02 22:09:24 +0545

    Added carousel for showing practise topics

--- ProjectNightingaleFrontend/src/app/practise/practise.component.ts
+++ ProjectNightingaleFrontend/src/app/practise/practise.component.ts
@@ -1,10 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Params, Router} from "@angular/router";
-import {map, Subscription, switchMap} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
+import {Subscription, switchMap} from "rxjs";
 import {RoutineService} from "../services/routine.service";
 import {IRoutine} from "../types/custom-interfaces";
-import {Routine} from "../models/routine-model/routine";
-
 @Component({
   selector: 'app-practise',
   templateUrl: './practise.component.html',
@@ -15,6 +13,8 @@ export class PractiseComponent implements OnInit{
   selectedRoutineSubscription: Subscription;
   loadedRoutine: IRoutine;
 
+  selectedTopicIndex = 0;
+
   constructor(private routineService: RoutineService, private route: ActivatedRoute) {
   }
 
@@ -24,4 +24,21 @@ export class PractiseComponent implements OnInit{
       .subscribe(routine => this.loadedRoutine = routine);
   }
 
+  onPrevClicked() {
+    if(this.selectedTopicIndex === 0){
+      this.selectedTopicIndex = 0 ;
+    }else {
+      this.selectedTopicIndex -= 1;
+    }
+  }
+
+  onNextClicked() {
+    if(this.loadedRoutine.topics){
+      if(this.selectedTopicIndex === this.loadedRoutine.topics?.length - 1){
+        this.selectedTopicIndex = this.loadedRoutine.topics?.length - 1;
+      }else {
+        this.selectedTopicIndex += 1;
+      }
+    }
+  }
 }
